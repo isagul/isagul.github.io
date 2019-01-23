4d9c0f571118b23df74915fc858caf67dd5d1a74
Isa Gul <isa.gul@beyn.com.tr>
2019-01-23 15:52:46 +0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# isagul.github.io
---
+++ precache-manifest.8cb8ea15691ac0313b2e8678103cde4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d73cd4382b0363660517",
+    "url": "/static/js/main.d73cd438.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "d73cd4382b0363660517",
+    "url": "/static/css/main.1160d2a9.chunk.css"
+  },
+  {
+    "revision": "181210c40e18b0c3b4d11b978432a552",
+    "url": "/index.html"
+  }
+];
