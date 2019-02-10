069d3b31b00615eaf43c842f956cb3bab0ce68fd
isagul <isagul3477@gmail.com>
2019-02-10 14:51:06 +0300

    Updates

---
+++ precache-manifest.ced2fd5836d1a12ac6ce157837842adc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4591ad9879f1084b13d4ecc08e2eaaf2",
+    "url": "/test/static/media/webpack.4591ad98.png"
+  },
+  {
+    "revision": "efad6a29f1d897df9bdf9011dfa9e8da",
+    "url": "/test/static/media/profile_image.efad6a29.jpg"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "3916262d3e78b0ba38a3",
+    "url": "/test/static/js/main.3916262d.chunk.js"
+  },
+  {
+    "revision": "f99ab3662670249c66d6",
+    "url": "/test/static/js/1.f99ab366.chunk.js"
+  },
+  {
+    "revision": "3916262d3e78b0ba38a3",
+    "url": "/test/static/css/main.620f2534.chunk.css"
+  },
+  {
+    "revision": "f8d527841c6d5da1ec7e4b76ade9de83",
+    "url": "/test/index.html"
+  }
+];
