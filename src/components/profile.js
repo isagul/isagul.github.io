f06ef05020dd58275942990481a44eeed5560788
isagul <isagul3477@gmail.com>
2019-02-13 00:09:53 +0300

    skills page was modified

--- src/components/profile.js
+++ src/components/profile.js
@@ -8,9 +8,10 @@ export default class Profile extends React.Component {
             <Animated animationIn="bounceInDown" animationOut="fadeOut" isVisible={true}>
                 <div className="profile">
                     <img src={require('../assets/images/profile_image.jpg')} alt="isa_gul_profile_image"/>
-                    <div>
+                    <div className="profile-info">
                         <p>İsa Gül</p>
                         <p>Jr. Front-End Developer</p>    
+                        <p>"I'm gonna start a revolution from my bed"</p>    
                     </div>
                 </div>   
             </Animated>
--- src/components/skills.js
+++ src/components/skills.js
@@ -16,65 +16,37 @@ export default class Skills extends React.Component {
         }
     }
     componentDidMount() {
-        this.jsInt = setInterval(() => {
-            this.setState({
-                jsBar: this.state.jsBar + 1
-            })
-        },10);
-        this.reactInt = setInterval(() => {
-            this.setState({
-                reactBar: this.state.reactBar + 1
-            })
-        },10);
-        this.cssInt = setInterval(() => {
-            this.setState({
-                cssBar: this.state.cssBar + 1
-            })
-        },10);
-        this.htmlInt = setInterval(() => {
-            this.setState({
-                htmlBar: this.state.htmlBar + 1
-            })
-        },10);
-        this.angularInt = setInterval(() => {
-            this.setState({
-                angularBar: this.state.angularBar + 1
-            })
-        },10);
-        this.sassInt = setInterval(() => {
-            this.setState({
-                sassBar: this.state.sassBar + 1
-            })
-        },10);
-        this.webpackInt = setInterval(() => {
-            this.setState({
-                webpackBar: this.state.webpackBar + 1
-            })
-        },10);
+        this.startInterval("jsInt","jsBar");
+        this.startInterval("reactInt","reactBar");
+        this.startInterval("cssInt","cssBar");
+        this.startInterval("htmlInt","htmlBar");
+        this.startInterval("angularInt","angularBar");
+        this.startInterval("sassInt","sassBar");
+        this.startInterval("webpackInt","webpackBar");
     }
 
 
     componentDidUpdate() {
-        if(this.state.jsBar === 80) {
-            clearInterval(this.jsInt);
-        }
-        if(this.state.reactBar === 70) {
-            clearInterval(this.reactInt);
-        }
-        if(this.state.cssBar === 75) {
-            clearInterval(this.cssInt);
-        }
-        if(this.state.htmlBar === 85) {
-            clearInterval(this.htmlInt);
-        }
-        if(this.state.angularBar === 75) {
-            clearInterval(this.angularInt);
-        }
-        if(this.state.sassBar === 85) {
-            clearInterval(this.sassInt);
-        }
-        if(this.state.webpackBar === 80) {
-            clearInterval(this.webpackInt);
+        this.stopInterval(this.state.jsBar, 80,this.jsInt);
+        this.stopInterval(this.state.reactBar, 70,this.reactInt);
+        this.stopInterval(this.state.cssBar, 75,this.cssInt);
+        this.stopInterval(this.state.htmlBar, 85,this.htmlInt);
+        this.stopInterval(this.state.angularBar, 75,this.angularInt);
+        this.stopInterval(this.state.sassBar, 85,this.sassInt);
+        this.stopInterval(this.state.webpackBar, 75,this.webpackInt);
+    }
+
+    startInterval = (intervalName, key) => {
+        this[intervalName] = setInterval(() => {
+            this.setState({
+                [key]: this.state[key] + 1
+            })
+        },10)
+    }
+
+    stopInterval = (skill, knowledgeValue,interval) => {
+        if(skill === knowledgeValue) {
+            clearInterval(interval);
         }
     }
 
@@ -83,56 +55,44 @@ export default class Skills extends React.Component {
         iconDiv.scrollIntoView({behavior: 'smooth'});
     }
 
+    
+
     render() {
+        const ProgressBar = (props) => {
+            const { barValue } = props;
+            return (
+                <div>
+                    <Line 
+                        percent={barValue}
+                        strokeWidth="1.3" 
+                        strokeColor="orange" 
+                        />
+                </div>
+            )
+        }
         return (
             <div className="skills">                               
                 <h3> <i className="fas fa-hand-point-right"></i>     
                     MY SKILLS</h3>
                 <div className="skill-progress">  
                     <i className="fab fa-html5"></i>
-                    <Line 
-                        style={{marginTop:'8px'}}
-                        percent={this.state.htmlBar}
-                        strokeWidth="1.3" 
-                        strokeColor="orange" /> <p>{this.state.htmlBar}%</p>  
+                    <ProgressBar barValue = {this.state.htmlBar} /> 
+                    <p>{this.state.htmlBar}%</p>
+                      
                     <i className="fab fa-css3-alt"></i>
-                    <Line 
-                        style={{marginTop:'8px'}}
-                        percent={this.state.cssBar}
-                        strokeWidth="1.3" 
-                        strokeColor="orange" /> <p>{this.state.cssBar}%</p>                
+                    <ProgressBar barValue = {this.state.cssBar} /> <p>{this.state.cssBar}%</p>                
                     <i className="fab fa-js"></i>
-                    <Line 
-                        style={{marginTop:'8px'}}
-                        percent={this.state.jsBar}
-                        strokeWidth="1.3" 
-                        strokeColor="orange" /> 
+                    <ProgressBar barValue = {this.state.jsBar} />
                     <p>{this.state.jsBar}%</p>
                     <i className="fab fa-react"></i>
-                    <Line 
-                        style={{marginTop:'8px'}}
-                        percent={this.state.reactBar}
-                        strokeWidth="1.3" 
-                        strokeColor="orange" /> 
+                    <ProgressBar barValue = {this.state.reactBar} />
                     <p>{this.state.reactBar}%</p>                            
                     <i className="fab fa-angular"></i>
-                    <Line 
-                        style={{marginTop:'8px'}}
-                        percent={this.state.angularBar}
-                        strokeWidth="1.3" 
-                        strokeColor="orange" /> <p>{this.state.angularBar}%</p>
+                    <ProgressBar barValue = {this.state.angularBar} /> <p>{this.state.angularBar}%</p>
                     <i className="fab fa-sass"></i>
-                    <Line 
-                        style={{marginTop:'8px'}}
-                        percent={this.state.sassBar}
-                        strokeWidth="1.3" 
-                        strokeColor="orange" /> <p>{this.state.sassBar}%</p>
+                    <ProgressBar barValue = {this.state.sassBar} /> <p>{this.state.sassBar}%</p>
                     <img src={require('../assets/images/webpack.png')} alt ="webpack_image"/>
-                    <Line 
-                        style={{marginTop:'8px'}}
-                        percent={this.state.webpackBar}
-                        strokeWidth="1.3" 
-                        strokeColor="orange" /> <p>{this.state.webpackBar}%</p>
+                    <ProgressBar barValue = {this.state.webpackBar} /> <p>{this.state.webpackBar}%</p>
                 </div>
             </div>
         )
