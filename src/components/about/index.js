cc80729d1a2f41dcd1840dd236a216ecf8de20c6
Isa Gul <isa.gul@beyn.com.tr>
2020-03-02 17:58:15 +0300

    new projects were added

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -13,9 +13,9 @@ const About = () => {
                         <span className="curly-braces">{" }"}</span>
                     </h4>
                     <p>I live in Istanbul. I am a Front End developer building websites and web applications.
-                        I interested in JavaScript and have professional experience working with Angular and React.
+                        I interested in JavaScript and have professional experience working with React and Angular.
                 </p>
-                    <p>I also have use these technologies: HTML/Sass, TypeScript, Gulp, Webpack, ES6</p>
+                    <p>Also, I have used these technologies: Redux, RxJS, Sass/Less, Gulp, Webpack, ES6</p>
                     {/* <a href="https://twitter.com/isamigul?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @isamigul</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>             */}
                 </div>
             </div>
